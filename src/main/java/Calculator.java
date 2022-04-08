a8db645b4ad3f599858798b0098f49f822164b1f
Meet Goswami <meet.goswami@iiitb.ac.in>
2022-04-08 12:23:47 +0530

    Final adding after all the steps completed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,25 +1,55 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Calculator {
+    private final Logger logger= LogManager.getLogger(Calculator.class);
     double getSqrt(int x)
     {
-        return Math.sqrt(x);
+        //logger.info("SQUARE ROOT : "+x);
+        logger.info("SQUARE ROOT : "+x);
+        double sqrt_ans=Math.sqrt(x);
+        //logger.info("RESULT:SQUARE ROOT("+x+") = "+sqrt_ans);
+        logger.info("RESULT:SQUARE ROOT("+x+") = "+sqrt_ans);
+        return sqrt_ans;
     }
     long getFact(int x)
     {
+        logger.info("FACTORIAL : "+x);
         long fact=1;
         for(int i=x;i>0;i--)
         {
             fact=fact*i;
         }
+        logger.info("RESULT:FACTORIAL("+x+") = "+fact);
         return fact;
     }
     double getLoge(int x)
     {
-        return Math.log(x);
+        logger.info("NATURAL LOGARITHM : "+x);
+        double loge_ans=0;
+        try {
+            if (x < 0) {
+                loge_ans = Double.NaN;
+                throw new ArithmeticException("Invalid input : Natural logarithm for negative values in not defined");
+            } else {
+                loge_ans = Math.log(x);
+            }
+        }
+        catch(ArithmeticException e) {
+            System.out.println("[EXCEPTION - LOG]"+e.getLocalizedMessage());
+
+
+        }
+        logger.info("RESULT:NATURAL LOGARITHM("+x+") = "+loge_ans);
+        return loge_ans;
     }
     double getPow(int x,int y)
     {
+        logger.info("POWER : BASE="+x+" EXPONENT="+y);
+        double pow_ans=Math.pow(x,y);
+        logger.info("RESULT : POWER("+x+","+y+") = "+pow_ans);
         return Math.pow(x,y);
     }
     public static void main(String args[])
@@ -27,8 +57,8 @@ public class Calculator {
         Scanner sca=new Scanner(System.in);
         int choice;
         Calculator calculator=new Calculator();
+        System.out.println("Scientific Calculator started...");
         do {
-            System.out.println("Scientific Calculator started...");
             System.out.println("1. Find Square Root");
             System.out.println("2. Find Factorial");
             System.out.println("3. Find Natural Logarithm");
